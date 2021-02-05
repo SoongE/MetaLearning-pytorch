b88d10c531ba774b58ab37002bdfdab19e735ed6
rhcsky <dev.rhcsky@gmail.com>
2021-02-05 14:31:00 +0900

    Change convolution block

--- prototypical/models/protonet.py
+++ prototypical/models/protonet.py
@@ -1,13 +1,5 @@
 import torch.nn as nn
-
-
-def conv_block(in_channels, out_channels):
-    return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, 3, padding=1),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d(2),
-    )
+from utils.conv_block import ConvBlock
 
 
 class ProtoNet(nn.Module):
@@ -18,10 +10,10 @@ class ProtoNet(nn.Module):
 
     def __init__(self, input_dim, hid_dim=64, z_dim=64):
         super(ProtoNet, self).__init__()
-        self.block1 = conv_block(input_dim, hid_dim)
-        self.block2 = conv_block(hid_dim, hid_dim)
-        self.block3 = conv_block(hid_dim, hid_dim)
-        self.block4 = conv_block(hid_dim, z_dim)
+        self.block1 = ConvBlock(input_dim, hid_dim, 3)
+        self.block2 = ConvBlock(hid_dim, hid_dim, 3)
+        self.block3 = ConvBlock(hid_dim, hid_dim, 3)
+        self.block4 = ConvBlock(hid_dim, z_dim, 3)
 
     def forward(self, x):
         out = self.block1(x)
