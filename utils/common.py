67f80ac8fc85f0cda6213c8d976735e11c1b368f
rhcsky <dev.rhcsky@gmail.com>
2021-02-15 16:47:04 +0900

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -53,4 +53,4 @@ def margin_of_error(values, confidence_interval=1.96):
     standard_deviation = math.sqrt(variance)
     standard_error = standard_deviation / math.sqrt(num)
 
-    return standard_error * confidence_interval
+    return mean, standard_error * confidence_interval
