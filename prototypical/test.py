c15f7447c25004927157d2326ec7ab84eabd4b50
rhcsky <dev.rhcsky@gmail.com>
2021-02-15 13:32:51 +0900

    Update test.py

--- prototypical/test.py
+++ prototypical/test.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 from glob import glob
+from pprint import pprint as pp
 from types import SimpleNamespace
 
 sys.path.append(os.path.dirname(os.path.realpath(os.path.dirname(__file__))))
@@ -33,38 +34,40 @@ def main():
 
     runs_path = glob('runs/*')
     max_len_exp = max([len(x) for x in runs_path])
-    print(f"|{'Experiment':^{max_len_exp}}|{'Loss':^10}|{'ACC'}:^10|")
+    print(f"|{'Experiment':^{max_len_exp}}|{'Loss':^10}|{'ACC':^10}|")
+
+    except_list = []
 
     for exp in glob('runs/*'):
         checkpoint, args = None, None
         files = glob(exp + '/*')
         for file in files:
             if file.endswith('model_best.pth'):
-                checkpoint = torch.load(file)
+                checkpoint = torch.load(os.path.abspath(file))
             elif file.endswith('.json'):
-                params = json.load(open(file))
+                params = json.load(open(os.path.abspath(file)))
                 args = SimpleNamespace(**params)
 
         if checkpoint is None or args is None:
-            print(f"checkpoint and params are not exist in {exp}")
+            except_list.append(f"checkpoint and params are not exist in {exp}")
             continue
 
-        test_loader = get_dataloader(args, args.dataset, 'test')
+        test_loader = get_dataloader(args, args.dataset, 'val')
 
         input_dim = 1 if args.dataset == 'omniglot' else 3
         if args.model == 'protonet':
             model = ProtoNet(input_dim).to(device)
-            print("ProtoNet loaded")
         else:
             model = ResNet(input_dim).to(device)
-            print("ResNet loaded")
 
         model.load_state_dict(checkpoint['model_state_dict'])
         best_acc1 = checkpoint['best_acc1']
 
         loss, acc = test(test_loader, model, criterion)
 
-        print(f"|{exp:^{max_len_exp}}|{loss:^10}|{acc}:^10|")
+        print(f"|{exp:^{max_len_exp}}|{loss:^10.3f}|{acc:^10.3f}|")
+
+    pp(except_list)
 
 
 @torch.no_grad()
