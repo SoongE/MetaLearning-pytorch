0296bd2ae56fe1d657caf4196a15d9995560a4a4
rhcsky <dev.rhcsky@gmail.com>
2021-02-17 13:02:36 +0900

    Update common.py

--- utils/common.py
+++ utils/common.py
@@ -54,3 +54,39 @@ def margin_of_error(values, confidence_interval=1.96):
     standard_error = standard_deviation / math.sqrt(num)
 
     return mean, standard_error * confidence_interval
+
+
+# from https://github.com/oscarknagg/few-shot
+def pairwise_distances(x, y, matching_fn):
+    """Efficiently calculate pairwise distances (or other similarity scores) between
+    two sets of samples.
+    # Arguments
+        x: Query samples. A tensor of shape (n_x, d) where d is the embedding dimension
+        y: Class prototypes. A tensor of shape (n_y, d) where d is the embedding dimension
+        matching_fn: Distance metric/similarity score to compute between samples
+    """
+    n_x = x.shape[0]
+    n_y = y.shape[0]
+
+    if matching_fn.lower() == 'l2' or matching_fn.lower == 'euclidean':
+        distances = (
+                x.unsqueeze(1).expand(n_x, n_y, -1) -
+                y.unsqueeze(0).expand(n_x, n_y, -1)
+        ).pow(2).sum(dim=2)
+        return distances
+    elif matching_fn.lower() == 'cosine':
+        normalised_x = x / (x.pow(2).sum(dim=1, keepdim=True).sqrt() + 1e-8)
+        normalised_y = y / (y.pow(2).sum(dim=1, keepdim=True).sqrt() + 1e-8)
+
+        expanded_x = normalised_x.unsqueeze(1).expand(n_x, n_y, -1)
+        expanded_y = normalised_y.unsqueeze(0).expand(n_x, n_y, -1)
+
+        cosine_similarities = (expanded_x * expanded_y).sum(dim=2)
+        return 1 - cosine_similarities
+    elif matching_fn.lower() == 'dot':
+        expanded_x = x.unsqueeze(1).expand(n_x, n_y, -1)
+        expanded_y = y.unsqueeze(0).expand(n_x, n_y, -1)
+
+        return -(expanded_x * expanded_y).sum(dim=2)
+    else:
+        raise (ValueError('Unsupported similarity function'))
