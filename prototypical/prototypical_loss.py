d41342171ddaf117b0175a4c156a46931a706da5
rhcsky <dev.rhcsky@gmail.com>
2021-02-17 13:02:21 +0900

    Update prototypical_loss.py

--- prototypical/prototypical_loss.py
+++ prototypical/prototypical_loss.py
@@ -2,6 +2,8 @@ import torch
 from torch.nn import functional as F
 from torch.nn.modules import Module
 
+from utils.common import pairwise_distances
+
 
 class PrototypicalLoss(Module):
     '''
@@ -14,24 +16,6 @@ class PrototypicalLoss(Module):
 
     def forward(self, input, target, n_support):
         return prototypical_loss(input, target, n_support, self.device)
-
-
-def euclidean_dist(x, y):
-    '''
-    Compute euclidean distance between two tensors
-    '''
-    # x: N x D
-    # y: M x D
-    n = x.size(0)
-    m = y.size(0)
-    d = x.size(-1)
-    if d != y.size(-1):
-        raise Exception
-
-    x = x.unsqueeze(1).expand(n, m, d)
-    y = y.unsqueeze(0).expand(n, m, d)
-
-    return torch.pow(x - y, 2).sum(2)
 
 
 def prototypical_loss(input, target, n_support, device):
@@ -63,11 +47,11 @@ def prototypical_loss(input, target, n_support, device):
     query_idxs = torch.stack(list(map(lambda c: target.eq(c).nonzero()[n_support:], classes))).view(-1)
     query_samples = input[query_idxs]
 
-    dists = euclidean_dist(query_samples, prototypes)
+    dists = pairwise_distances(query_samples, prototypes, 'l2')
 
     log_p_y = F.log_softmax(-dists, dim=1)
     y_hat = log_p_y.argmax(1)
-    target_label = torch.LongTensor([x for x in range(n_classes) for _ in range(n_query)]).to(device)
+    target_label = torch.arange(0, n_classes, 1 / n_query).long().to(device)
 
     loss_val = torch.nn.NLLLoss()(log_p_y, target_label)
     acc_val = y_hat.eq(target_label.squeeze()).float().mean()
