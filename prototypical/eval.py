5351f26c9fae0d6c883cfc1ef00225b300d6f268
rhcsky <dev.rhcsky@gmail.com>
2021-02-19 15:55:22 +0900

    Update eval.py

--- prototypical/eval.py
+++ prototypical/eval.py
@@ -17,12 +17,16 @@ from models.resnet import ResNet
 from prototypical_loss import PrototypicalLoss
 
 from utils.common import margin_of_error
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
 
 best_acc1 = 0
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 parser = argparse.ArgumentParser()
 args = parser.parse_args()
+summary = SummaryWriter()
 
 
 def main():
@@ -100,7 +104,66 @@ def test(test_loader, model, criterion):
         losses.append(loss)
         top1.append(acc1)
 
+        print(y_pred)
+        print(y)
+        # probs = [torch.nn.functional.softmax(el, dim=0)[i].item() for i, el in zip(y, y_pred)]
+        #
+        # summary.add_figure('predictions vs. actuals',
+        #                    plot_classes_preds(predict_labels, probs, [sample_images, test_images],
+        #                                       [sample_labels, test_labels], [sample_class, test_class]),
+        #                    global_step=episode + 1)
+        # write_plot_to_tensorboard = False
+
     return losses, top1
+
+
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+def plot_classes_preds(preds, probs, images, labels, N_way, K_shot):
+    """
+    Plot Prediction Samples.
+    Parameters
+    ----------
+    preds : list
+        contain prediction value range from 0 to N_way - 1.
+    probs : list
+        contain prediction probability range from 0.0 ~ 1.0.
+    images : list
+        images[0] is sample images. Shape is (N_way, K_shot, 1, 28, 28).
+        images[1] is query images. Shape is (N_way, K_shot, 1, 28, 28).
+    labels: list
+        labels[0] contains y value for sample image.
+        labels[1] contains y value for query image.
+    """
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(30, 20))
+    sample_images, query_images = images
+    # display sample images
+    for row in np.arange(K_shot):
+        for col in np.arange(N_way):
+            ax = fig.add_subplot(2 * K_shot, N_way, row * N_way + col + 1, xticks=[], yticks=[])
+            matplotlib_imshow(sample_images[col * K_shot + row], one_channel=True)
+            ax.set_title(labels[0][col * K_shot + row].item())
+    # display query images
+    for row in np.arange(K_shot):
+        for col in np.arange(N_way):
+            ax = fig.add_subplot(2 * K_shot, N_way, N_way * K_shot + row * N_way + col + 1, xticks=[], yticks=[])
+            matplotlib_imshow(query_images[col * K_shot + row], one_channel=True)
+            ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+                preds[col * K_shot + row],
+                probs[col * K_shot + row] * 100.0,
+                labels[1][col * K_shot + row]),
+                color=("green" if preds[col * K_shot + row] == labels[1][col * K_shot + row].item() else "red"))
+    return fig
 
 
 if __name__ == '__main__':
